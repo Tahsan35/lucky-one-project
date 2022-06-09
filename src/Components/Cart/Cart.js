97a135dd4a10b35e62f59c33fc0bcac9b49965a6
Tahsan35 <tahsanknmas14@gmail.com>
2022-06-09 09:09:10 +0600

    complete but not

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
+    const [item, setItem] = useState([]);
+    const [filterItem, setFilterItem] = useState([]);
+
+    useEffect(() => {
+        const items = cart;
+        if (item.length === 4) {
+            alert('Oops you already selected 4 products'); 
+        } 
+        else {
+            setItem(items);
+        }
+    }, [cart]);
+
+    // filter function
+    const filterOne = () => {
+            if (item.length !== 0) {
+                const filter = item[(Math.floor(Math.random() * item.length))];
+                setFilterItem(filter);
+                item.length = 0;
+            }
+    }
+    // remove Item 
+    const removeItem = (productToRemove) => {
+        setItem(
+            item.filter((product) => product !== productToRemove)
+        );
+        item.length = 0;
+    }
+    
     let name='';
     for (const product of cart) {
         name=name+product.name
-    }
-    console.log(cart)
+    };
     return (
         <div className='cart'>
-            <h1>hellow from cart</h1>
-                <h3>{name}</h3>
+            <h1>choice any 4 shoes</h1>
+            <h3 className='text'>{name}</h3>
+            <button onClick={filterOne} className='btn-success'>Choose one</button>
         </div>
+        
     );
 };
 
